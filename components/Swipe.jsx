bac3f826c890bc639ce021d10e432d33db8b7760
HelenOne <h.bright@yandex.ru>
2020-03-22 01:11:42 +0300

    added rate-requests and card when found:false

--- components/Swipe.jsx
+++ components/Swipe.jsx
@@ -24,13 +24,20 @@ export const Swipe = () => {
       });
     //  .finally(() => setLoading(false));
   }, []);
-
+  const [dataIsTrue, setDataIsTrue] = useState(true);
   const [book, setBook] = useState(undefined);
   const loadBook = React.useCallback(() => {
     fetch(`${config.apiUrl}/book?user_id=${store.userId}`, { method: "get" })
       .then(data => data.json())
       .then(data => {
-        setBook(JSON.parse(data.book));
+        data === true
+          ? setBook(JSON.parse(data.book))
+          : setBook({
+              imageLink:
+                "https://cdn.pixabay.com/photo/2018/02/17/17/33/sorry-3160426_960_720.png",
+              description:
+                "Sorry, you have watched all the books recommended to you. You can use searching now"
+            }) && setDataIsTrue(false);
       })
       .catch(err => {
         console.error(err);
@@ -42,9 +49,34 @@ export const Swipe = () => {
     //  .finally(() => setLoading(false));
   }, []);
   React.useEffect(loadBook, []);
+  const sendRate = React.useCallback(
+    isGood => {
+      const formData = new FormData();
+      formData.append("user_id", store.userId.toString());
+      formData.append("book_id", book.id);
+      formData.append("good", isGood);
+      fetch(`${config.apiUrl}/rate`, {
+        method: "post",
+        body: formData
+      })
+        .then(() => {
+          setSubmitted(true);
+          setLoading(false);
+        })
+        .catch(() => {
+          // setLoading(false);
+          notification.open({
+            message: "Our server is being down 🤯",
+            description: "Our team of the web monkeys are on the way to fix it"
+          });
+        });
+    },
+    [book]
+  );
   const handleOnSwipe = swipeDirection => {
     if (swipeDirection === direction.RIGHT) {
       // handle right swipe
+      sendRate(true);
       setCardNumber(cardNumber + 1);
       if (cardNumber > welcomeCards.length) loadBook();
       return;
@@ -52,11 +84,13 @@ export const Swipe = () => {
 
     if (swipeDirection === direction.LEFT) {
       // handle left swipe
+      sendRate(false);
       setCardNumber(cardNumber + 1);
       if (cardNumber > welcomeCards.length) loadBook();
       return;
     }
   };
+
   return (
     <div className={styles.cardContainer}>
       <Swipeable onSwipe={handleOnSwipe}>
@@ -88,30 +122,35 @@ export const Swipe = () => {
                   size={200}
                   src={book.imageLink}
                 />
-                <div
-                  className={styles.title}
-                >{`${book.author}: ${book.title}`}</div>
+                <div className={styles.title}>
+                  {" "}
+                  {book.author ? `${book.author}: ${book.title}` : " "}
+                </div>
                 {book.description && (
                   <div className={styles.description}>{book.description}</div>
                 )}
-                <Button
-                  shape="round"
-                  size="large"
-                  type="ghost"
-                  onClick={() => {
-                    const existingBook = store.cart.find(
-                      item => item.book.id === book.id
-                    );
-                    if (!existingBook) {
-                      store.cart.push({ book, amount: 1 });
-                    } else {
-                      existingBook.amount++;
-                    }
-                    store.cartVisible = true;
-                  }}
-                >
-                  BUY ({book.price}$)
-                </Button>
+                {!dataIsTrue ? (
+                  <Button
+                    shape="round"
+                    size="large"
+                    type="ghost"
+                    onClick={() => {
+                      const existingBook = store.cart.find(
+                        item => item.book.id === book.id
+                      );
+                      if (!existingBook) {
+                        store.cart.push({ book, amount: 1 });
+                      } else {
+                        existingBook.amount++;
+                      }
+                      store.cartVisible = true;
+                    }}
+                  >
+                    BUY ({book.price}$)
+                  </Button>
+                ) : (
+                  <div>{"\n"}</div>
+                )}
               </div>
             )}
           </div>
